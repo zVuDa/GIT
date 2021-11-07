1bfc4658ff4b2f7830aa3320d79b1b4a34b2e0db
xurxogonzalez <info@webferrol.com>
2021-11-07 22:17:22 +0100

    Ejercicio de Ejemplo de recorrido de Array y css en Bootstrap

---
+++ js/app.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+ * document.querySelectorAll  Para seleccionar los artículos
+ * document.querySelector Para los botones
+ * 
+ * nodo.addEventListener
+ * 
+ * for(){
+ * }
+ * 
+ * while(){
+ * }
+ * 
+ * do{
+ * }while();
+ */
